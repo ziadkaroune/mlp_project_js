45fcb72e39f0187973ac7852f28646de950af9e0
Ziad Karoune <ziad99kr@gmai.com>
2026-09-11 13:07:55 +0200

    default args

---
+++ utils/DefaultArgs.js
@@ -0,0 +1,17 @@
+
+
+
+export const DefaultArgs = {
+    train: 'data_train.csv',
+    valid: 'data_valid.csv',
+    layer: ['24', '24'],
+    activation: 'sigmoid',
+    weights_initializer: 'heUniform',
+    epochs: '70',
+    learning_rate: '0.03',
+    batch_size: '8',
+    loss: 'categoricalCrossentropy',
+    seed: '42',
+    out: 'saved_model.json',
+    curves_out: 'learning_curves.svg',
+  };
