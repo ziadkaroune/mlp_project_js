4edcae6ad92d10f8282da5208aee3da4aa2c55e6
Ziad Karoune <ziad99kr@gmai.com>
2026-09-16 15:55:35 +0200

    add transpose methode in Matrix Clas

--- class/Matrix.js
+++ class/Matrix.js
@@ -1,41 +1,58 @@
 
 export default class Matrix {
-        columns;
-        rows;
-        data ;
-        constructor(coord){
-          this.data = coord.map(el => [...el]);
-          this.columns = this.data[0].length;
-          this.rows  =  this.data.length;
-        }    
 
- 
-    printm(){
-        return this.data;
+    columns;
+    rows;
+    data ;
+
+    constructor(coord){
+                this.data = coord.map(el => [...el]);
+                this.columns = this.data[0].length;
+                this.rows  =  this.data.length;
+    }    
+
+    size(){
+        return {
+            rows : this.rows ,
+            colums : this.columns
+        }
     }
+  
     add(mat){
         
-        const datab = this.data.map((row) => [...row]);
+        const datab = this.data;
       //  this.equal_matrix_size(mat);
 
         for(let x = 0 ; x < this.rows ; x++){
              for(let y = 0 ; y < this.columns ; y++)
                     datab[x][y]  +=  mat.data[x][y];
         }
-        return new Matrix(datab).printm();
+              return new Matrix(datab);
     }
       sub(mat){
         
-        const datab = this.data.map((row) => [...row]);
+        const datab = this.data;
       //  this.equal_matrix_size(mat);
 
         for(let x = 0 ; x < this.rows ; x++){
              for(let y = 0 ; y < this.columns ; y++)
                     datab[x][y]  -=  mat.data[x][y];
         }
-        return new Matrix(datab).printm();
+        return new Matrix(datab);
     }
 
+ 
+    transpose(){
+
+        let transpose_arr = Array.from(({length : this.columns}) , ()=>
+        new Array(this.rows).fill(0));
+        
+        for(let x = 0 ; x < this.rows ; x++) {
+                for(let y = 0 ; y < this.columns ;y++ )
+                        transpose_arr[y][x] = this.data[x][y];
+        }
+        return new Matrix(transpose_arr);
+    }
  
 
 }
